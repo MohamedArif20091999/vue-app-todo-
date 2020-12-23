66e0acc8ba0c72cc27450e0dae3d71e6e5eaed00
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2020-12-23 20:44:38 +0530

    appbar and card initiated

---
+++ src/components/Appbar.vue
@@ -0,0 +1,5 @@
+<template>
+   <v-app-bar app color="lime" dark>
+      <v-toolbar-title class="brown--text">Todo App</v-toolbar-title>
+    </v-app-bar>
+</template>
---
+++ src/components/Todoview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <v-card >
+            <v-card-title>
+                Title
+            </v-card-title>
+            <v-card-subtitle>Todo body</v-card-subtitle>
+        </v-card>
+    </div>
+</template>
+
+<style scoped>
+    .v-card{
+        max-width: 70%;
+       
+        margin: auto;
+        
+    }
+
+</style>
